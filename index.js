4a487dd1eda38c7519ad60b3e062c405eb30265d
Atul Kumar Pandey <pandeyatul0095@gmail.com>
2022-02-03 19:24:05 +0530

    Added buttons, logo, button functionalities
    Added three buttons (classic, rainbow, erase), also added their functions. Classic button changes background of box to black, rainbow button changes it to random color and erase button resets boxes to white background. Also added hovering glowing effect to buttons.

--- index.js
+++ index.js
@@ -3,16 +3,59 @@ let container = document.querySelector("#container");
 for(let i=0;i <16;i++){
     for(let j=0;j <i;j++){
         let columns = document.createElement('div'); 
-        columns.classList.add('columns');
+        columns.classList.add('box');
         container.appendChild(columns);
         let rows = document.createElement('div'); 
-        rows.classList.add('rows');
+        rows.classList.add('box');
         container.appendChild(rows);
     }
 }
-    
-const divs = document.querySelectorAll('div');
-console.log(divs);
-divs.forEach((div) => {
-  div.style.setProperty('--div-background-color', 'skyblue');
-});
+
+let classicBtn = document.querySelector("#classic-btn");
+  classicBtn.addEventListener("click", classicChange);
+
+
+function classicChange(){
+  let boxs = document.querySelectorAll('.box');
+    boxs.forEach(box =>  {
+    box.style.backgroundColor = "white";
+  });
+    boxs.forEach(box => box.addEventListener('mouseover', function change(){
+    box.style.backgroundColor = "black";
+  }))
+}
+
+let eraseBtn = document.querySelector("#erase-btn");
+  eraseBtn.addEventListener("click", erase);
+  
+function erase(){
+  let boxs = document.querySelectorAll('.box');
+    boxs.forEach(box =>  {
+    box.style.backgroundColor = "white";
+    box.addEventListener('mouseover', function change(){
+    box.style.backgroundColor = "white";
+    })
+})}
+
+let rainbowBtn = document.querySelector("#rainbow-btn");
+  rainbowBtn.addEventListener("click", rainbowChange);
+
+function rainbowChange(){
+  let boxs = document.querySelectorAll('.box');
+    boxs.forEach(box =>  {
+    box.style.backgroundColor = "white";
+      });
+    boxs.forEach(box => box.addEventListener('mouseover', function change(){
+    color = getRandomColor();
+    box.style.backgroundColor = color;
+      }))
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+      }
+    return color;
+    }
